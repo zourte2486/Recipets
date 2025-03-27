83e043310f46319296660e0adc6592f6e5ada04e
zourte2486 <zourte2486@gmail.com>
2025-03-27 02:06:02 +0100

    Add ingredient submission

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
     <div className="container">
       <Header />
       <Content />
+      
     </div>
   );
 }
--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,14 +1,31 @@
 const Content = () => {
+  const ingredients = ["tomato", "onion", "lettuce", "cucumber"];
+
+
+
+  const handelSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const newIngredient = formData.get("ingredient");
+    ingredients.push(newIngredient);
+    console.log(ingredients);
+  }
+  const ingredientsList = ingredients.map((ingredient, index) => (
+    <li key={index}>{ingredient}</li>
+  ));
   return (
     <main>
-      <form className="add-ingredient-form">
+      <form onSubmit={handelSubmit} className="add-ingredient-form">
         <input
           type="text"
           placeholder="e.g. oregano"
           aria-label="Add ingredient"
+          name="ingredient"
         />
         <button>Add ingredient</button>
+        
       </form>
+      <ul>{ingredientsList}</ul>
     </main>
   );
 };
