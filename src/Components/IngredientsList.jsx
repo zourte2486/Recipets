03354997bb21b27074f1300669d67294b35d1b42
zourte2486 <zourte2486@gmail.com>
2025-04-10 01:32:41 +0100

    Refactor application structure

---
+++ src/Components/IngredientsList.jsx
@@ -0,0 +1,18 @@
+export default function IngredientsList(props) {
+    const ingredientsListItems = props.ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+    return (
+        <section>
+            <h2>Ingredients on hand:</h2>
+            <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
+            {props.ingredients.length > 3 && <div className="get-recipe-container">
+                <div>
+                    <h3>Ready for a recipe?</h3>
+                    <p>Generate a recipe from your list of ingredients.</p>
+                </div>
+                <button onClick={props.toggleRecipeShown}>Get a recipe</button>
+            </div>}
+        </section>
+    )
+}
---
+++ src/Main.jsx
@@ -0,0 +1,42 @@
+import React from "react"
+import IngredientsList from "./Components/IngredientsList"
+import ClaudeRecipe from "./Components/ClaudeRecipe"
+
+export default function Main() {
+    const [ingredients, setIngredients] = React.useState(
+        ["all the main spices", "pasta", "ground beef", "tomato paste"]
+    )
+    const [recipeShown, setRecipeShown] = React.useState(false)
+
+    function toggleRecipeShown() {
+        setRecipeShown(prevShown => !prevShown)
+    }
+
+    function addIngredient(formData) {
+        const newIngredient = formData.get("ingredient")
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+    }
+
+    return (
+        <main>
+            <form action={addIngredient} className="add-ingredient-form">
+                <input
+                    type="text"
+                    placeholder="e.g. oregano"
+                    aria-label="Add ingredient"
+                    name="ingredient"
+                />
+                <button>Add ingredient</button>
+            </form>
+
+            {ingredients.length > 0 &&
+                <IngredientsList
+                    ingredients={ingredients}
+                    toggleRecipeShown={toggleRecipeShown}
+                />
+            }
+
+            {recipeShown && <ClaudeRecipe />}
+        </main>
+    )
+}
