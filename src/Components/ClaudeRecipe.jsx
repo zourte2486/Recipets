e55d2788443a9a7983b06aab4aa2f822a0e59a14
zourte2486 <zourte2486@gmail.com>
2025-04-11 00:45:25 +0100

    Refactor ClaudeRecipe component

--- src/Components/ClaudeRecipe.jsx
+++ src/Components/ClaudeRecipe.jsx
@@ -1,9 +1,10 @@
-
+import ReactMarkdown from "react-markdown"
 
 export default function ClaudeRecipe(props) {
     return (
-        <section>
-            {props.recipe}
+        <section className="suggested-recipe-container" aria-live="polite">
+            <h2>Chef Claude Recommends:</h2>
+            <ReactMarkdown>{props.recipe}</ReactMarkdown>
         </section>
     )
 }
